f00a7fd270ba3245a2f2840795c15724bfb6233c
dRamee <maksim.yolckin@yandex.com>
2022-10-15 21:24:23 +0700

    Task 4 and 5

---
+++ task4.py
@@ -0,0 +1,18 @@
+test_voc = {"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832, "Max": 97832}
+
+# test_voc = {}
+# l = int(input())
+#
+# for i in range(l):
+#     voc_key, voc_value = input().split()
+#     voc_value = int(voc_value)
+#     test_voc[voc_key] = voc_value
+
+new_voc = {}
+for key, value in test_voc.items():
+    if value not in new_voc.keys():
+        new_voc[value] = (key, )
+    else:
+        new_voc[value] = (*new_voc[value], key)
+
+print(new_voc)
---
+++ task5.py
@@ -0,0 +1,17 @@
+def specialize(func, *args, **kwargs):
+    def newfunc(*args1, **kwargs1):
+        return func(*args, *args1, **{**kwargs, **kwargs1})
+    return newfunc
+
+
+
+def my_sum(x, y):
+    return x+y
+
+
+# plus_one = specialize(my_sum, 1)
+
+
+
+print(plus_one(2))
+
