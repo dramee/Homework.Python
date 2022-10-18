6365dd60ddaacfdd9a649fddfcbe8e75fa847c0b
dRamee <maksim.yolckin@yandex.com>
2022-10-19 01:59:17 +0700

    fix tuple

--- task4.py
+++ task4.py
@@ -11,8 +11,12 @@ test_voc = {"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832, "Max": 97832}
 new_voc = {}
 for key, value in test_voc.items():
     if value not in new_voc.keys():
-        new_voc[value] = (key, )
+        new_voc[value] = key
     else:
-        new_voc[value] = (*new_voc[value], key)
-
+        if isinstance(new_voc[value], tuple):
+            new_voc[value] = (*new_voc[value], key)
+        else:
+            new_voc[value] = (new_voc[value], key)
 print(new_voc)
+
+
