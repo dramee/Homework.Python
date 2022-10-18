9f148be6ea7e8f1439d8b4ab22182550da8fef39
dRamee <maksim.yolckin@yandex.com>
2022-10-19 02:01:07 +0700

    remove extra dict

--- task5.py
+++ task5.py
@@ -1,6 +1,6 @@
 def specialize(func, *args, **kwargs):
     def newfunc(*args1, **kwargs1):
-        return func(*args, *args1, **{**kwargs, **kwargs1})
+        return func(*args, *args1, **kwargs, **kwargs1)
     return newfunc
 
 
@@ -11,8 +11,8 @@ def my_sum(x, y):
 
 # plus_one = specialize(my_sum, 1)
 
-just_two = specialize(my_sum, 1, 1)
+# just_two = specialize(my_sum, 1, 1)
 
-# print(plus_one(2))
+# print(plus_one(10))
 
-print(just_two())
+# print(just_two())
