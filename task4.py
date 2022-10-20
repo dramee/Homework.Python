2ce3f08eba3ac579f34a9c4d1f17257b78ac229f
dRamee <maksim.yolckin@yandex.com>
2022-10-21 01:36:31 +0700

    One more try

--- task4.py
+++ task4.py
@@ -1,22 +1,12 @@
-test_voc = {"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832, "Max": 97832}
-
-# test_voc = {}
-# l = int(input())
-#
-# for i in range(l):
-#     voc_key, voc_value = input().split()
-#     voc_value = int(voc_value)
-#     test_voc[voc_key] = voc_value
+test_voc = {"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832, "Max": 97832, ((102, 301), 1): 'Aaaa'}
 
 new_voc = {}
 for key, value in test_voc.items():
     if value not in new_voc.keys():
         new_voc[value] = key
+    elif not(isinstance(new_voc[value], tuple)) or new_voc[value] in test_voc.keys():
+        new_voc[value] = (new_voc[value], key)
     else:
-        if isinstance(new_voc[value], tuple):
-            new_voc[value] = (*new_voc[value], key)
-        else:
-            new_voc[value] = (new_voc[value], key)
+        new_voc[value] = (*new_voc[value], key)
+
 print(new_voc)
-
-
